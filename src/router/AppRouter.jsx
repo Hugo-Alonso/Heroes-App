6ed933268d27bc6a9f6bf43b993a243a6d1fceb0
Hugo Alonso <hugoalonsotoledo@gmail.com>
2024-12-15 17:48:25 +0100

    Router modified

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import { MarvelPage } from "../heroes/pages/MarvelPage"
 import { LoginPage } from "../auth/pages/LoginPage"
 import { DcPage } from "../heroes/pages/DCPage"
+
 import { Navbar } from "../ui"
 
 export const AppRouter = () => {
