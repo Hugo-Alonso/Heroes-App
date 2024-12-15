dd76450f17883357bb97059d430b2f900d3af23d
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2024-12-15 17:15:58 +0100

    Upload project

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,23 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+
+import { MarvelPage } from "../heroes/pages/MarvelPage"
+import { LoginPage } from "../auth/pages/LoginPage"
+import { DcPage } from "../heroes/pages/DCPage"
+import { Navbar } from "../ui"
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Navbar />
+            
+            <Routes>
+                <Route path="marvel" element={<MarvelPage />} />
+                <Route path="dc" element={<DcPage />} />
+
+                <Route path="login" element={<LoginPage />} />
+
+                <Route path="/" element={<Navigate to="/marvel" />} />            
+            </Routes> 
+        </>
+    )
+}
