fc78bb1c7c834a89a8809037877f8da4ae3bf2be
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2024-12-17 18:37:29 +0100

    Add text and characters for HeroCard

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 export const HeroCard = ({heroe}) => {
 
-    const { id, superhero, publiisher, alter_ego, first_appearance, characters } = heroe;
+    const { id, superhero, publisher, alter_ego, first_appearance, characters } = heroe;
+
+    const heroImageUrl = `/assets/heroes/${ id }.jpg`
 
     return( 
         <>
@@ -10,8 +12,18 @@ export const HeroCard = ({heroe}) => {
                 <div className="card">
                     <div className="row no gutters">
                         <div className="col-4">
-                            <img src="" alt={ superhero } className="card-img" />
+                            <img src={heroImageUrl} alt={ superhero } className="card-img" />
                         </div>
+
+                        <div className="col-8">
+                            <div className="card-body">
+                                <h5 cardName="card-title">{ superhero }</h5>
+                                <p className='card-text'>{ alter_ego }</p>
+
+                                <p>{ characters }</p>      
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
