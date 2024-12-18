61f22e67e24f41787b05448fc789c530bd064d96
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2024-12-18 12:09:12 +0100

    Correct path not recognized

--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -3,8 +3,8 @@ import { HeroList } from "../components"
 export const MarvelPage = () => {
   return (
     <>
-      <h1>MarvelPage</h1>
-      <hr />    
+      <h1 className="mt-3">MarvelPage</h1>
+      <hr/>    
 
       <HeroList publisher="Marvel Comics"/>
     </>
