527168ae2dfffca35a235056e420045338603998
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2024-12-18 15:42:22 +0100

    Query-String package add

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { HeroCard } from '../components'
 import { useForm } from '../../hooks/useForm';
 import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
 
 export const SearchPage = () => {
 
