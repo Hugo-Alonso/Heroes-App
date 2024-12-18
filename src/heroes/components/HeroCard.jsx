d43973e27d2a107d1c7bf4f922779098f0769486
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2024-12-18 09:46:31 +0100

    HeroCard modified

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+
 import { Link } from 'react-router-dom';
+
+const CharactersByHero = ({ alter_ego, characters}) => {
+    // if (alter_ego === characters ) return <></>;
+    // return <p>{ characters }</p>
+
+    return (alter_ego === characters) 
+        ? <></>
+        : <p>{ characters }</p>
+}
 
 export const HeroCard = ({heroe}) => {
 
     const { id, superhero, publisher, alter_ego, first_appearance, characters } = heroe;
 
     const heroImageUrl = `/assets/heroes/${ id }.jpg`
+
+    const charactersByHero = (<p>{ characters }</p>)
 
     return( 
         <>
@@ -21,9 +33,11 @@ export const HeroCard = ({heroe}) => {
                                 <h5 className="card-title">{ superhero }</h5>
                                 <p className='card-text'>{ alter_ego }</p>
 
-                                {
-                                    ( alter_ego !== characters) && (<p>{ characters }</p>)
-                                }    
+                                {/* {
+                                    ( alter_ego !== characters) && charactersByHero
+                                }*/}
+
+                                <CharactersByHero characters={ characters } alter_ego={ alter_ego } />
 
                                 <p className='card-text'>
                                     <small className='text-muted'>{ first_appearance }</small>    
