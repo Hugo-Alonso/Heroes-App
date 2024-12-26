9cc14ce6a0128cc3efa4191d66f48463dfa322d5
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2024-12-26 12:46:02 +0100

    User active although reload page

--- src/auth/context/authReducer.js
+++ src/auth/context/authReducer.js
@@ -1,13 +1,14 @@
 import { types } from "../types/types";
 
-export const authReducer = ( state = { }, action ) => {
+export const authReducer = ( state = {}, action ) => {
 
     switch ( action.type ) {
+
         case types.login:
             return {
                 ...state,
                 logged: true,
-                user: action.payload.name   
+                user: action.payload 
             };
 
         case types.logout:
